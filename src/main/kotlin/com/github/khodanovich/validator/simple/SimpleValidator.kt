c6ecd8b5601665006b2dcfee38afe8afcb4b0aba
Alexsandr <alex-migon@mail.ru>
2019-01-01 21:37:40 +0500

    feature/add_generic_builder_scope

--- src/main/kotlin/com/github/khodanovich/validator/simple/SimpleValidator.kt
+++ src/main/kotlin/com/github/khodanovich/validator/simple/SimpleValidator.kt
@@ -3,14 +3,14 @@ package com.github.khodanovich.validator.simple
 import com.github.khodanovich.validator.Validator
 
 
-class SimpleValidator<V>: Validator<V> {
+class SimpleValidator<Value> : Validator<Value> {
 
-    override var predicate: ((V)->Boolean)? = null
-    var action: ((V) -> Unit)? = null
+    override var predicate: ((Value)->Boolean)? = null
+    var action: ((Value) -> Unit)? = null
     var error: (()-> Unit)? = null
 
     override fun check(value: Any?): Boolean{
-        val checkedValue = value as V
+        val checkedValue = value as Value
         predicate?.let {
             if (it.invoke(value)){
                 action?.invoke(checkedValue)
